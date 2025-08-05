baad1c2e5fddd262cdbc93abb67ada4f569a0430
Hebatullah Dwairi <hebamahmoud8843@gmail.com>
2025-08-05 20:28:28 +0300

    .

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python manage.py migrate
+python manage.py collectstatic --no-input
+
+python manage.py mqtt_listener &
+
+gunicorn your_project_name.wsgi:application --bind 0.0.0.0:$PORT
